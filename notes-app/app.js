1bae6245cf2d72c420b4aea171b90d53e7c1e2a2
Pranav <prnvbirajdar@gmail.com>
2021-06-21 09:50:08 -0400

    yargs exercice 1

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,5 @@
-const yargs= require('yargs');
+const { argv } = require('yargs');
+const yargs = require('yargs');
 
 //  const chalk = require('chalk')
 
@@ -11,14 +12,68 @@ const yargs= require('yargs');
 
 //console.log(chalk.green('Hello world'));
 
-console.log(process.argv);
 
-console.log(yargs.argv);
+// YARGS COMMAND 
+
+// Add
+
+yargs.command({
+    command: 'add',
+    describe: 'Adds a new note',
+    builder: {
+        title: {
+            describe: 'Notes title',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'Notes body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function () {
+        console.log('notes title: ', argv.title);
+        console.log('notes body: ', argv.body);
+
+    }
+})
+
+// Remove
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removes an existing note',
+    handler: function () {
+        console.log('note removed');
+    }
+})
+
+
+// List
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists all existing notes',
+    handler: function () {
+        console.log('notes list');
+    }
+})
+
+
+// Read
+
+yargs.command({
+    command: 'read',
+    describe: 'Reads a note',
+    handler: function () {
+        console.log('note read');
+    }
+})
 
 
 
-
-
+yargs.parse()
 
 // CREATING FILE, WRITING, AND APPENDING STUFF TO TEXT DOC
 
