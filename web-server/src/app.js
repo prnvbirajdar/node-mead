fc22034dc0a8e009deb7efff15222276faf7e49a
Pranav <prnvbirajdar@gmail.com>
2021-09-11 19:01:35 -0400

    using hbs to server dynamic stuff

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,11 +3,30 @@ const express = require('express')
 
 const app = express()
 
-console.log(__dirname);
-
+// paths for express config
 const publicPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
 
+// setup handlebars and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath)
+
+// serving static directory
 app.use(express.static(publicPath))
+
+app.get('', (req, res)=>{
+    res.render('index', {
+        title: 'dynamic hbs',
+        name: 'Pranav'
+    })
+})
+
+app.get('/hello', (req, res)=>{
+    res.render('hello', {
+        title: 'dynamic hbs',
+        name: 'Pranav Birajdar'
+    })
+})
 
 // app.get('/help', (req, res)=>{
 //     res.send('hello express')
