f320709387f6c81944f7c11b15f70a07df904ec8
Pranav <prnvbirajdar@gmail.com>
2021-09-12 19:25:15 -0400

    404 pages, template partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // paths for express config
 const publicPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // setup handlebars and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // serving static directory
 app.use(express.static(publicPath))
@@ -28,6 +31,13 @@ app.get('/hello', (req, res)=>{
     })
 })
 
+app.get('/blog', (req, res)=>{
+    res.render('blog', {
+        title: 'blog title',
+        name: 'Pranav Birajdar'
+    })
+})
+
 // app.get('/help', (req, res)=>{
 //     res.send('hello express')
 // })
@@ -38,6 +48,25 @@ app.get('/hello', (req, res)=>{
 
 app.get('/weather', (req, res)=>{
     res.send('weather')
+})
+
+
+// 404 pages
+
+app.get('/blog/*', (req, res)=> {
+    res.render('404', {
+        message: 'blog article not found',
+        title: 'blog 404 page',
+        name: 'Pranav Birajdar'
+    })
+})
+
+app.get('*', (req, res)=> {
+    res.render('404', {
+        message: 'page not found',
+        title: '404 page',
+        name: 'Pranav Birajdar'
+    })
 })
 
 app.listen(3000, ()=>{
