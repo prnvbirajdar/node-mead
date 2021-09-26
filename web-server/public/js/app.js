763a6f244904b8fab5c59e6327cb7bd9d2d8a9ce
Pranav <prnvbirajdar@gmail.com>
2021-09-26 17:16:03 -0400

    fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,10 @@
 console.log('hi from js folder');
+
+fetch('http://localhost:3000/weather?address=boston').then(response => {
+    response.json().then(({error, forecast, place} = {})=>{
+        if(error){
+            return console.log(error);
+        }
+        return console.log(place, forecast);
+    })
+})
