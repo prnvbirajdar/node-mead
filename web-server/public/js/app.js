f7cd90f6e28591af0ea993f03fcb95b564aa6315
Pranav <prnvbirajdar@gmail.com>
2021-09-26 19:58:15 -0400

    input and submit html

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,10 +1,22 @@
 console.log('hi from js folder');
 
-fetch('http://localhost:3000/weather?address=boston').then(response => {
-    response.json().then(({error, forecast, place} = {})=>{
-        if(error){
-            return console.log(error);
-        }
-        return console.log(place, forecast);
+const weatherForm = document.querySelector('form')
+const weatherInput = document.querySelector('input')
+
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    let location = weatherInput.value
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+        response.json().then(({error, forecast, place} = {})=>{
+            if(error){
+                return console.log(error);
+            }
+            return console.log(place, forecast);
+        })
     })
+
+    weatherInput.value = ''
+    
 })
